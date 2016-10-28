7629bfbc4dae50d38904b7d0aef85e83aa3bd97e
David Lieberman <dlieberman@hbs.edu>
2016-10-27 20:44:31 -0400

    Draft of user generator

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,35 @@ Route::get('/lorem/{count}', function($count) {
 
     return $output;
 });
+
+Route::get('/users/{count}', function($count) {
+    $males = file('./txt/male_names.txt');
+    $females = file('./txt/female_names.txt');
+    $surnames = file('./txt/last_names.txt');
+
+    $output = "";
+    for ($i=0; $i<$count; $i++) {
+      // Male or female_names
+      $gender = mt_rand(0,100) % 2;
+
+      // Prefix? (odds: 1 in 20)
+
+      // Pick gender-appropriate first name
+      $firstNames = $gender ? $males : $females;
+      $r = mt_rand(0, count($firstNames) - 1);
+      $output.=trim($firstNames[$r]);
+
+      // Pick last name
+      $r = mt_rand(0, count($surnames)-1);
+      $output.=trim($surnames[$r]);
+
+      // Suffix (odd: 1 in 50);
+
+      // Next
+      $output.="<br/>";
+
+    }
+
+    return $output;
+
+});
