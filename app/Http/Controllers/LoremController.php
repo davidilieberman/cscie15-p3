fefff6304d6aae4715e3d6bc198db6ea412a7c59
David Lieberman <dlieberman@hbs.edu>
2016-10-30 15:33:37 -0400

    Modularized code for functional clarity.

--- app/Http/Controllers/LoremController.php
+++ app/Http/Controllers/LoremController.php
@@ -8,17 +8,13 @@ use Barryvdh\Debugbar\Facade as Debugbar;
 
 class LoremController extends Controller {
 
-  function generate(Request $request) {
+  /**
+  Given the requested number of paragraphs, return an array of
+  strings representing a randomized passaged of Lorem Ipsum text. 
+  */
+  function buildPassage($count) {
 
-    // Validate the form input values
-    $this->validate($request, [
-      'loremCount' => 'required|numeric|min:1|max:99'
-    ]);
-
-    // Define the number of requested paragraphs
-    $count = $request->input('loremCount');
-
-    // Load the source file into memory
+    // Load the Lorem Ipsum source file into memory
     $lines = file('./txt/lorem.txt');
 
     // Initialize the output array
@@ -32,6 +28,30 @@ class LoremController extends Controller {
       // The Laravel helper function array_pull helps ensure we don't repeat any paragraphs in our output
       array_push($arr, array_pull($lines, $r));
     }
+
+    return $arr;
+
+  }
+
+  /**
+  Given a request for a passage of Lorem-Ipsum text, verify that the request provides
+  a valid number of paragraphs and, if so, generate the passage.
+  */
+  function generate(Request $request) {
+
+    // Validate the form input values
+    $this->validate($request, [
+      'loremCount' => 'required|numeric|min:1|max:99'
+    ]);
+
+    // Define the number of requested paragraphs
+    $count = $request->input('loremCount');
+
+    // Load the source file into memory
+    $lines = file('./txt/lorem.txt');
+
+    // Construct the passage
+    $arr = $this->buildPassage($count);
 
     // Return the result, and send the user input argument back to be echoed in the form display
     return view('lorem.generate')
